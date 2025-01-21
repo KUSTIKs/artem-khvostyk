4ef9005b068e198869a674bd352753cb8546bf05
Artem Khvostyk <artem.khvostyk.email@gmail.com>
2025-01-21 21:17:00 +0100

    feat: add size to button

--- src/components/core/button/button.tsx
+++ src/components/core/button/button.tsx
@@ -6,13 +6,24 @@ import classes from './button.module.scss';
 type Props = ComponentPropsWithRef<'button'> & {
   asChild?: boolean;
   variant?: 'primary' | 'ghost' | 'outlined';
+  size?: 'base' | 'sm';
 };
 
-const Button = ({ asChild, variant = 'primary', ...props }: Props) => {
+const Button = ({
+  asChild,
+  variant = 'primary',
+  size = 'base',
+  ...props
+}: Props) => {
   const Component: ElementType = asChild ? Slot : 'button';
 
   return (
-    <Component className={classes.button} data-variant={variant} {...props} />
+    <Component
+      className={classes.button}
+      data-variant={variant}
+      data-size={size}
+      {...props}
+    />
   );
 };
 
