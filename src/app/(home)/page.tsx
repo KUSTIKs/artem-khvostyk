82b252e3fab9d57d7fc176a6d0e514c1b8811b6e
Artem Khvostyk <artem.khvostyk.email@gmail.com>
2025-01-21 19:05:52 +0100

    feat: set up linting, add css variables

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import styles from './home.module.css';
+
+const HomePage = () => {
+  return (
+    <main className={styles.page}>
+      <h1>Hello, world</h1>
+    </main>
+  );
+};
+
+export default HomePage;
